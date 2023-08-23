8cbb57ef0c10885d6cee2b7281b82accf96a1697
Ailton Antônio de Araújo <ailton.a.araujo@gmail.com>
2023-08-23 13:53:26 -0300

    Implementação do Binary Search

---
+++ src/example/search/test/binarySearch.test.js
@@ -0,0 +1,17 @@
+/*
+*  Aplicação realiza a busca binária por um elemento de um array de inteiros
+*/
+import binarySearch from "../binarySearch.js";
+
+var array = [];
+
+beforeAll(() => {
+     array = binarySearch.initialize(10);
+});
+
+describe('Verificando a incialização', () => {
+    test('Array contem de 1 a X elementos', () => {
+        expect(array.length > 0 && array.length <= 10).toBe(true);
+    });
+
+});
